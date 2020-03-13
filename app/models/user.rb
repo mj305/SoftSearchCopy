a0b3c26b2fe5b449bd3aad4c84069a59b290d40c
ElvisHernandez <ElvisHernandezTheOne@gmail.com>
2020-03-13 02:44:53 -0400

    added the dependent destroy condition to the User Job association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_many :jobs
+  has_many :jobs, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
