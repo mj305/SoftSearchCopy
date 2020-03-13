19104234cb5f60614bc3e3d4ed8502d90e22c683
ElvisHernandez <ElvisHernandezTheOne@gmail.com>
2020-03-12 23:48:24 -0400

    added the devise User model with employer attribute

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_03_12_231214) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.boolean "employer", default: false, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
