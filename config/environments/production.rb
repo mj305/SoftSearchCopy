a05197a7aa8956146e7fd704b4b12e3262ea75ff
mj305 <maria.j.beckles@gmail.com>
2020-03-13 10:42:47 -0400

    added bootstrap to devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "final_project_template_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: ENV['HOST'] }
+
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
