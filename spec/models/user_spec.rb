bcf25b696428b3c356cca5df63fcded7c8307e6b
ElvisHernandez <ElvisHernandezTheOne@gmail.com>
2020-03-14 18:46:07 -0400

    added attribute tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  subject {
+    described_class.new(email: "Anything@gmail.com",
+                        password: "Loremh"
+                        )
+  }
+
+  it "is valid with valid attributes" do 
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a email" do
+    subject.email = nil
+    expect(subject).to_not be_valid 
+  end
+
+  it "is not valid without a password" do
+    subject.password = nil
+    expect(subject).to_not be_valid
+  end
+
+  
 end
