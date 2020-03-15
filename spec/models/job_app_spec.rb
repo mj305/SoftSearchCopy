7bb9c41a2c62150460a53a212198cfb4f33482ff
ElvisHernandez <ElvisHernandezTheOne@gmail.com>
2020-03-15 18:27:55 -0400

    generated job_app spec

---
+++ spec/models/job_app_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+require 'user_data'
+
+RSpec.describe JobApp, type: :model do
+  
+  subject {
+    described_class.new(
+      user_id: User.create(email:"blahblah@yahoo.com",password:"123456", employer:false).id,
+      job_id: Job.create(
+        position: "sr. dev",
+        description: "Loremh",
+        date: "02/03/2020",
+        longitude: -80.2,
+        latitude:45.1,
+        user_id: User.create(email:"blah@yahoo.com",password:"123456", employer:true).id 
+                        ).id
+                        )
+  }  
+end
