41e4e9445451ed97db80736792b3a817d45e960c
ElvisHernandez <ElvisHernandezTheOne@gmail.com>
2020-03-16 02:24:40 -0400

    added User and Job association tests in user_favorite_spec

--- app/models/user_favorite.rb
+++ app/models/user_favorite.rb
@@ -1,5 +1,6 @@
 class UserFavorite < ApplicationRecord
-  validates :user, uniqueness: { scope: :job }
-  belongs_to :job
-  belongs_to :user, -> { where employer:false } 
+  validates :user_id, uniqueness: { scope: :job_id } , presence: true
+  validates :job_id, presence: true
+  belongs_to :job, foreign_key: 'job_id'
+  belongs_to :user, -> { where employer:false }, foreign_key: 'user_id' 
 end
--- spec/models/user_favorite_spec.rb
+++ spec/models/user_favorite_spec.rb
@@ -18,4 +18,55 @@ RSpec.describe UserFavorite, type: :model do
                         )
   }  
 
+  describe "Validations" do
+    
+    it "is valid with valid attributes" do 
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without a user_id" do
+      should validate_presence_of(:user_id)
+    end
+
+    it "is not valid without a job_id" do
+      should validate_presence_of(:job_id)
+    end
+
+    it "should not have multiple identical user_ids and job_ids" do 
+      should validate_uniqueness_of(:user_id).scoped_to(:job_id)
+    end
+
+    it "is not valid with a user_id that belongs to a user where employer:true" do
+      employer_user = UserData::employer
+      employer_jobs = UserData::jobs(employer_user.id)
+      employer_apps = UserData::job_apps(employer_user.id,employer_jobs)
+      expect(employer_apps.length).to eq 0
+    end
+  end
+
+  describe "User Associations" do
+    
+    it "should belong to a unique user where { employer: false }" do
+      should belong_to(:user).
+      with_foreign_key('user_id').
+      conditions(employer:false) 
+    end
+
+    it "should be able to access the user associated with it" do
+      expect(subject.user.email).to eq "blahblah@yahoo.com"
+    end
+  end
+
+  describe "Job Associations" do
+    
+    it "should belong to a unique job" do
+      should belong_to(:job).
+      with_foreign_key('job_id')
+    end
+
+    it "should be able to access the job associated with it" do
+      expect(subject.job.position).to eq "sr. dev"
+    end
+  end
+
 end
