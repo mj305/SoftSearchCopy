c88f237cb485e0f5290f4e0d0ca84607de55f990
ElvisHernandez <ElvisHernandezTheOne@gmail.com>
2020-03-16 21:38:10 -0400

    removed pages view

--- app/javascript/components/NavigationHome.jsx
+++ app/javascript/components/NavigationHome.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 // import logo from './logo.png';
 
-const Navigation = () => {
+const Navigation = ({ isSignedIn }) => {
   return (
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3">
     <h5 class="my-0 mr-md-auto font-weight-normal">kdpmfss Logo</h5>
@@ -9,9 +9,14 @@ const Navigation = () => {
       <a class="p-2 text-white px-4" href="#">Find Jobs</a>
       <a class="p-2 text-white" href="#">Post a Job</a>
     </nav>
-    <a id="login" class="btn btn-outline-primary mr-3" href="#">Login</a>
-    <a id="signup" class="btn btn-primary" href="#">Sign up</a>
-  </div>
+    {isSignedIn ? 
+      (<a id="login" class="btn btn-outline-primary mr-3" href='/users/sign_out'>Logout</a>) :
+      <>
+      <a id="login" class="btn btn-outline-primary mr-3" href='/users/sign_in'>Login</a>
+      <a id="signup" class="btn btn-primary" href='/users/sign_up'>Sign up</a>
+      </>
+    }  
+    </div>
   );
 };
 
