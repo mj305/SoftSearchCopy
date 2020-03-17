961ad44afbfe8b02c1ee7a58efa1a8c381e26363
mj305 <maria.j.beckles@gmail.com>
2020-03-16 22:06:23 -0400

    modal setup for signin and signup pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
-    
+    #before_action :authenticate_user!
+    #before_action :ensure_applicant_user!
+
     def home 
     end
 
@@ -7,5 +9,20 @@ class PagesController < ApplicationController
         query = params['q']
         puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! in the pages controller!!!#{params['q']}"
         redirect_to controller: :maps, action: :show, location: query
+    end
+
+    def trigger_signin
+    end
+
+    def trigger_signup
+    end
+
+
+
+    
+    def ensure_applicant_user!
+        unless current_user and current_user.employer == false
+            redirect_to root_path
+        end
     end
 end
