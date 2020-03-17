a779ea0083fdcc11943ecdb7f12331c6c9b5714e
ElvisHernandez <ElvisHernandezTheOne@gmail.com>
2020-03-17 02:26:41 -0400

    added job markers with a specified radius

--- app/javascript/components/Map.jsx
+++ app/javascript/components/Map.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 
-const Map = ({API_KEY, coords}) => {
+const Map = ({API_KEY, coords, jobs}) => {
 
     const usCenter = [-98.5795,39.8283] // center of the united states
 
@@ -9,19 +9,42 @@ const Map = ({API_KEY, coords}) => {
         height: "500px"
     }
 
+    function filterGeoJsonPoints(points) {
+        var center = coords;
+        var radius = 500;
+        var options = {steps: 64, units: 'miles', properties: {foo: 'bar'}};
+        var circle = turf.circle(center, radius, options);
+        var geoPoints = turf.points(points)
+        return( turf.pointsWithinPolygon(geoPoints, circle).features )
+      }
+
+    function geoJsonPoints(array) {
+        let points = array.map( ({longitude,latitude}) => {
+            return [longitude,latitude]
+        })
+        // console.log(points)
+        return points //points is an array of 2D LngLat arrays
+    }
+
     useEffect(() => {
         mapboxgl.accessToken = API_KEY;
         var map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/dark-v10',
-        center: usCenter,
-        zoom: 3
+        center: coords,
+        zoom: 6
         });
         
         var marker = new mapboxgl.Marker({color: '#a83232'})
-        .setLngLat(usCenter)
+        .setLngLat(coords)
         .addTo(map)
-
+    
+        filterGeoJsonPoints(geoJsonPoints(jobs))
+        .forEach(({geometry}) => {
+          new mapboxgl.Marker()
+          .setLngLat(geometry.coordinates)
+          .addTo(map);
+        })
 
         // add geolocate control to the map
 
