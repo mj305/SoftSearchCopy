16a3525603e9d18ece9513787c4fdc0c7eb46fe2
ElvisHernandez <ElvisHernandezTheOne@gmail.com>
2020-03-17 05:28:37 -0400

    display job positions below map corresponding to map markers

--- app/javascript/components/Map.jsx
+++ app/javascript/components/Map.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Map = ({API_KEY, coords, jobs}) => {
+    const [filteredJobs, setFilteredJobs] = useState([])
 
     const usCenter = [-98.5795,39.8283] // center of the united states
 
@@ -9,15 +10,42 @@ const Map = ({API_KEY, coords, jobs}) => {
         height: "500px"
     }
 
+    // var from = turf.point(coords);
+    // var to = turf.point([-80.1373,26.1224]);
+    // var options = {units: 'miles'};
+    // var distance = turf.distance(from, to, options);
+
+    // returns points within a specified radius
+    // function filterGeoJsonPoints(points) {
+    //     var center = coords;
+    //     var radius = 500;
+    //     var options = {steps: 64, units: 'miles', properties: {foo: 'bar'}};
+    //     var circle = turf.circle(center, radius, options);
+    //     var geoPoints = turf.points(points)
+    //     return( turf.pointsWithinPolygon(geoPoints, circle).features )
+    //   }
+
     function filterGeoJsonPoints(points) {
         var center = coords;
         var radius = 500;
         var options = {steps: 64, units: 'miles', properties: {foo: 'bar'}};
         var circle = turf.circle(center, radius, options);
         var geoPoints = turf.points(points)
-        return( turf.pointsWithinPolygon(geoPoints, circle).features )
+        var filteredPoints = turf.pointsWithinPolygon(geoPoints, circle).features
+        jobsInRange(filteredPoints)
+        return(filteredPoints)
       }
 
+      function jobsInRange(geoPoints) {
+         setFilteredJobs(jobs.filter( job => {
+              return(geoPoints.some( ({geometry}) => {
+                  return(geometry.coordinates[0] === job.longitude 
+                    && geometry.coordinates[1] === job.latitude)
+              }))
+          }))
+      }
+
+    // takes a json type object and returns an array of [longitude, latitude] sub arrays
     function geoJsonPoints(array) {
         let points = array.map( ({longitude,latitude}) => {
             return [longitude,latitude]
@@ -25,6 +53,7 @@ const Map = ({API_KEY, coords, jobs}) => {
         // console.log(points)
         return points //points is an array of 2D LngLat arrays
     }
+
 
     useEffect(() => {
         mapboxgl.accessToken = API_KEY;
@@ -57,16 +86,20 @@ const Map = ({API_KEY, coords, jobs}) => {
                 showAccuracyCircle: false
             })
         )
-
-
-
-
-
     },[])
     return(
         <div>
             <h1>Hello React!</h1>
             <div id='map' style={style}></div>
+            <div>
+                {filteredJobs.map( job => {
+                    return(
+                        <div>
+                            <h1>{job.position}</h1>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
