89f998af844c033543ec94b130289682c6a1d2be
leopolicastro <lpolicastro@pm.me>
2020-03-18 14:44:29 -0400

    allow employer parameter on sign up and fix broken devise views

--- app/controllers/applicants/user_base_controller.rb
+++ app/controllers/applicants/user_base_controller.rb
@@ -1,11 +1,12 @@
-class Applicants::UserBaseController < ApplicationController
-    before_action :authenticate_user!
-    before_action :ensure_applicant_user!
+# frozen_string_literal: true
 
-    
-    def ensure_applicant_user!
-        unless current_user and current_user.employer == false
-            redirect_to root_path
-        end
+class Applicants::UserBaseController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_applicant_user!
+
+  def ensure_applicant_user!
+    unless current_user && (current_user.employer == false)
+      redirect_to root_path
+    end
 end
 end
