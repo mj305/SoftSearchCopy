f0c9dea1f1a8ddb9a671547c5d624e59a31edcf6
ElvisHernandez <ElvisHernandezTheOne@gmail.com>
2020-03-19 00:33:40 -0400

    added seed data for skill and jobskill models

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,4 @@
 class Skill < ApplicationRecord
-    validates: :name, presence: true, uniqueness: true
+    validates :name, presence: true, uniqueness: true
     has_many :job_skills, dependent: :destroy
 end
