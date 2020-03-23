7dc1dc9209a6690cff273738e96f1f008332c514
ElvisHernandez <ElvisHernandezTheOne@gmail.com>
2020-03-22 22:33:46 -0400

    added user_id authentication to job app pages

--- app/controllers/applicants/job_applications_controller.rb
+++ app/controllers/applicants/job_applications_controller.rb
@@ -1,4 +1,14 @@
 class Applicants::JobApplicationsController < Applicants::UserBaseController
+    before_action :is_current_user
     def index
+    end
+
+    private
+
+    def is_current_user
+        user_url_id = params['user_id'].to_i
+        unless current_user.id == user_url_id
+            redirect_to root_path
+        end
     end
 end
